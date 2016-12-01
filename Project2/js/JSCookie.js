b2f16863b66dd81035aa65ec7cd35fb519c3cc92
Alan S <alstew13@hotmail.com>
2016-12-01 20:38:35 +0000

    Added Podcast page, Edited Footer
    Added a podcast page that has links to soundcloud embeded. Also added
    social media icons to footer with working links

--- Project2/js/JSCookie.js
+++ Project2/js/JSCookie.js
@@ -16,8 +16,8 @@ function init () {
 	}
 
 	console.log(userName);
-	document.getElementById('cookieuserName').innerHTML = userName;
-	document.getElementById('cookieEmail').value = userName;
+	document.getElementById('name').innerHTML = userName;
+	document.getElementById('jcEmail').value = userName;
 
 }
 
@@ -25,14 +25,14 @@ function processForm(){
 
 	console.log('Processing Form');
 	document.getElementById('outName').innerHTML =  document.getElementById('name').value
-	document.getElementById('outEmail').innerHTML =  document.getElementById('userEmail').value
+	document.getElementById('outEmail').innerHTML =  document.getElementById('jcEmail').value
 
 	var expireDate = new Date();
 
 	expireDate.setMonth(expireDate.getMonth()+12);
 
 	setCookie('name',document.getElementById('name').value, '/', expireDate.toGMTString());
-	setCookie('email',document.getElementById('userEmail').value, '/', expireDate.toGMTString());
+	setCookie('email',document.getElementById('jcEmail').value, '/', expireDate.toGMTString());
 
 }
 
@@ -50,7 +50,7 @@ function setCookie (name, value, path, expireDate) {
 	// some more detail, such as an expiry date, and the URL to which it applies.  This function
 	// requires a value for the Cookie Key, the Cookie Value, time in months to expiry, and the URL to
 	// which it applies.  The full text to be supplied is of the form:
-	//                           SOMEKEY=SOMEVALUE;path=SOMEPATH;expires=SOMEDATE 
+	//                           SOMEKEY=SOMEVALUE;path=SOMEPATH;expires=SOMEDATE
 
 	console.log('setting cookie')
 	document.cookie = name +'=' + value + ';path=' + path + ';expires=' + expireDate;
@@ -61,14 +61,14 @@ function setCookie (name, value, path, expireDate) {
 function getCookie(c_name){
 
 	// a cookie is really just a string with some rules on how it is to be constructed
-	// so in order to test for the existence of a cookie, and/or retrive its value we 
+	// so in order to test for the existence of a cookie, and/or retrive its value we
 	// need to look at some string manipulation.  The code below is adapted from W3Schools
-	
+
 	// first the existance of the cookie is checked; if the cookie does not exist, null is returned
 	// As the cookie is really just a string, we simply need to select the part of the string that we
 	// are interested in, by using substring, and return that.
-	// to do this we need to use the substring method, which will return a string, dependent on the 
-	// values for start and end.   
+	// to do this we need to use the substring method, which will return a string, dependent on the
+	// values for start and end.
 
 	console.log('getting cookie')
 
@@ -96,7 +96,7 @@ function getCookie(c_name){
 
 }
 
-// Some code from a previous example to attach the event handler to the button 
+// Some code from a previous example to attach the event handler to the button
 
 function addEvent (obj, listener, handler) {
 	if (obj){
@@ -110,7 +110,6 @@ function addEvent (obj, listener, handler) {
 		}
 	}
 	else {
-		console.log("Element " + obj + " not found");	
+		console.log("Element " + obj + " not found");
 	}
 }
-
